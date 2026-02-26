a012403e2459dc16aab80dea61d97679a89f0faa
coolsik-park <coolsik.park@gmail.com>
2026-02-26 14:56:09 +0900

    fix: use proper vite base path for github pages repo

--- vite.config.js
+++ vite.config.js
@@ -2,5 +2,5 @@ import { defineConfig } from 'vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    base: './', // Ensures assets are loaded with relative paths for GitHub Pages
+    base: '/docent-sample/', // Set to the specific repository name for GitHub Pages to resolve assets correctly
 })
